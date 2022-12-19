e8be73f0f202baa564dfd9ad722a5bf63ce2bd6b
Martin Siregar <72garmartin@gmail.com>
2022-12-19 17:18:25 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = hapi.server({
+    port: 8008,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+init();
